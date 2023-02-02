255a5e0c2106a6eb0438b9c0ce903d1bc22068c0
sher85 <32082005+sher85@users.noreply.github.com>
2023-02-01 22:31:22 -0600

    added form with location and animal dropdown

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
+  // Hooks
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
 
   return (
     <div className="search-params">
       <form>
+        {/* LOCATION */}
         <label htmlFor="location">
           Location
           <input
@@ -15,6 +20,23 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+
+        {/* ANIMAL */}
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+
+        {/* BUTTON */}
         <button>Submit</button>
       </form>
     </div>
