1476daf92ebf3e87be4c696f174c16d32d8df27c
sher85 <32082005+sher85@users.noreply.github.com>
2023-02-01 22:50:21 -0600

    updated form to include breed

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+// Temporary list of breeds until we plug in the API
+const breeds = [];
 
 const SearchParams = () => {
   // Hooks
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
 
   return (
     <div className="search-params">
@@ -27,11 +30,29 @@ const SearchParams = () => {
           <select
             id="animal"
             value={animal}
-            onChange={(e) => setAnimal(e.target.value)}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
           >
             <option />
             {ANIMALS.map((animal) => (
               <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        {/* BREED */}
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            disabled={breeds.length === 0}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
             ))}
           </select>
         </label>
