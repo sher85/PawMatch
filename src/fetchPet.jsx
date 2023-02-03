fae9b7ea9b5db3b60f9d23b903ae92c93e100ac0
sher85 <32082005+sher85@users.noreply.github.com>
2023-02-02 21:46:47 -0600

    added react-query api call ability

--- src/fetchPet.jsx
+++ src/fetchPet.jsx
@@ -1,4 +1,4 @@
-const fetchPet = async ({ query }) => {
+const fetchPet = async ({ queryKey }) => {
   const id = queryKey[1];
   const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
 
