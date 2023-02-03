9848eac47e10f430304878c3b6f21be9f2aecedc
sher85 <32082005+sher85@users.noreply.github.com>
2023-02-03 12:02:12 -0600

    file paths changed to reflect moving of react components to components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+import Details from "./components/Details";
+import SearchParams from "./components/SearchParams";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
