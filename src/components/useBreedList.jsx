94109ff8cf2acb42ed8a5645dc5a85aed9140d16
sher85 <32082005+sher85@users.noreply.github.com>
2023-02-03 18:52:04 -0600

    refactor code for retrieving breed list using react query

--- src/components/useBreedList.jsx
+++ src/components/useBreedList.jsx
@@ -1,42 +1,9 @@
-import { useState, useEffect } from "react";
-
-const localCache = {};
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "../fetchRequests/fetchBreedList";
 
 // Custom hook to manage breed list state and fetch data from API
 export default function useBreedList(animal) {
-  const [breedlist, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
+  const results = useQuery(["breeds", animal], fetchBreedList);
 
-  // Effect to fetch breed list data from API or retrieve from local cache
-  useEffect(() => {
-    // If animal value is not provided -> clear breed list
-    if (!animal) {
-      setBreedList([]);
-      // If animal value is in local cache -> retrieve from cache
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-      // If animal value is not in local cache, fetch from API
-    } else {
-      requestBreedList();
-    }
-
-    async function requestBreedList() {
-      setBreedList([]);
-      setStatus("loading");
-
-      // API Request: Breed list data for a specific animal
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-
-      const json = await res.json();
-
-      // Update local cache with new data and set breed list to cache value
-      localCache[animal] = json.breeds || [];
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    }
-  }, [animal]);
-
-  return [breedlist, status];
+  return [results?.data?.breeds ?? [], results.status];
 }
