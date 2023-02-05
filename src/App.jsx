558f9c04410ac1c7cd1f4c38aea80c1c6c401a08
sher85 <32082005+sher85@users.noreply.github.com>
2023-02-05 16:50:41 -0600

    changed logic to accomodate server side rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // Static Imports
 import { useState, lazy, Suspense } from "react";
-import { createRoot } from "react-dom/client";
 import { Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -49,7 +48,4 @@ const App = () => {
   );
 };
 
-// Locate root and deploy application
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
